b695ef7535254abdd28361d745e913ae0f8403a7
omar <omarlhweis9641@gmail.com>
2025-08-31 09:48:20 -0400

    message

---
+++ weatherReport/weather_report.js
@@ -0,0 +1,20 @@
+function showweatherDetails(event) {
+    event.preventDefault();
+    const city = document.getElementById('city').value;
+    const apiKey = 'c26e739e64c4802856bc2ed8dc9f0a39'; // Replace 'YOUR_API_KEY' with your actual API key
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      const weatherInfo = document.getElementById('weatherInfo');
+      weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+                              <p>Temperature: ${data.main.temp} &#8451;</p>
+                              <p>Weather: ${data.weather[0].description}</p>`;
+    })
+    .catch(error => {
+        console.error('Error fetching weather:', error);
+        const weatherInfo = document.getElementById('weatherInfo');
+        weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
+      });
+}
+document.getElementById('weatherForm').addEventListener('submit',showweatherDetails );
